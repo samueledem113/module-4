ec0b95e96d5d1bedf57b1cf958231738c66c84d2
samueledem113 <139284030+samueledem113@users.noreply.github.com>
2024-06-27 16:03:06 +0000

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Array of names
+var speakWord = "Good Bye";
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
+var speakWord = "Hello";
+
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
